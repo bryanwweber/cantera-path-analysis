9cc773a4e5c8bb843439058328fd791f32aae287
Bryan W. Weber <bryan@bryanwweber.com>
2015-10-23 17:27:35 -0400

    Rename package canpathan
    The project name is CanPathAn

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='Cantera Path Analysis',
+    name='CanPathAn',
     version='0.0.1',
-    packages=['cantera-path-analysis'],
+    packages=['canpathan'],
     entry_points={
       'console_scripts': [
-          'cantera-path-analysis = cantera-path-analysis.__main__:main'
-      ]
+          'cantera-path-analysis = canpathan.__main__:main',
+      ],
     },
 )
