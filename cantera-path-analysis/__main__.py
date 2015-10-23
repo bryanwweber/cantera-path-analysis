1ac0da3f113e184773743acf581480b626d15f54
Bryan W. Weber <bryan@bryanwweber.com>
2015-10-23 14:26:43 -0400

    Add argument parser and package __init__

---
+++ cantera-path-analysis/__main__.py
@@ -0,0 +1,26 @@
+import argparse as arg
+from .path_analysis import PathAnalysis
+
+
+def main():
+    parser = arg.ArgumentParser(
+        description="Create a path analysis file from a CanSen output file"
+        )
+    parser.add_argument('-c', '--chem', help='Specify the chemistry file', default='gri30.cti')
+    parser.add_argument('-s', '--save', help='Specify the stored save file', default='save.hdf')
+    parser.add_argument('-o', '--output', help='Specify the output file', default='output.xlsx')
+    parser.add_argument('-p', '--percent', help='Specify the integration percent',
+                        default=20.0, type=float)
+    parser.add_argument('fuel', help='Specify the fuel in the mixture')
+    args = parser.parse_args()
+
+    PathAnalysis(
+        chem=args.chem,
+        save=args.save,
+        output=args.output,
+        percent=args.percent,
+        fuel=args.fuel,
+        )
+
+if __name__ == '__main__':
+    main()
