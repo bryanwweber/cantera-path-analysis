5df858535b2e0ceb7ca563559ad9cc2f563de532
Bryan W. Weber <bryan@bryanwweber.com>
2016-05-12 13:53:09 -0400

    Rewrite PathAnalysis to use sparse matrices
    This should improve the speed relative to looping and the memory usage
    relative to vectorized operation

--- pychempath/path_analysis.py
+++ pychempath/path_analysis.py
@@ -1,78 +1,74 @@
 from cantera import Solution
 from pandas import DataFrame
+import scipy.sparse as sps
 import numpy as np
 import tables
+from collections import defaultdict
 
 
 class PathAnalysis(object):
     def __init__(self, chem, save, output, percent, fuel):
-        self.chem_file_name = chem
-        self.save_file_name = save
-        self.output_file_name = output
+        chem_file_name = chem
+        save_file_name = save
+        output_file_name = output
         self.conversion_percent = percent
         self.fuel = fuel
-        self.read_save_file()
-        self.generate_table()
-        self.write_output()
 
-    def read_save_file(self):
-        with tables.open_file(self.save_file_name, mode='r') as save_file:
+        with tables.open_file(save_file_name, mode='r') as save_file:
             table = save_file.root.reactor
             self.time = table.cols.time[:]
             self.mass_fractions = table.cols.mass_fractions[:]
             self.temperature = table.cols.temperature[:]
             self.pressure = table.cols.pressure[:]
 
-    def get_mole_fractions(self, gas):
-        avg_molar_mass = 1/np.sum(self.mass_fractions/gas.molecular_weights, axis=1)
-        return (self.mass_fractions.T*avg_molar_mass).T/gas.molecular_weights
+        gas = Solution(chem_file_name)
+        self.n_reactions = gas.n_reactions
+        self.reaction_equations = gas.reaction_equations()
+        self.species_names = gas.species_names
+        self.stoich_diff = gas.product_stoich_coeffs() - gas.reactant_stoich_coeffs()
+        self.molecular_weights = gas.molecular_weights
 
-    def get_conversion_indices(self, mole_fractions):
+        self.conversion_indices = self.get_conversion_indices()
+        self.rate_of_production = self.generate_rate_of_production(gas)
+        self.integ_rop = self.generate_table()
+        self.integ_rop.to_excel(output_file_name)
+
+    def get_mole_fractions(self):
+        avg_molar_mass = 1/np.sum(self.mass_fractions/self.molecular_weights, axis=1)
+        return (self.mass_fractions.T*avg_molar_mass).T/self.molecular_weights
+
+    def get_conversion_indices(self):
+        mole_fractions = self.get_mole_fractions()
         mask = mole_fractions[0, :] > 0
         molar_conversion = (1.0 - mole_fractions[:, mask]/mole_fractions[0, mask])*100
         initial_species_names = np.array(self.species_names)[mask]
         fuel_index = np.where(initial_species_names == self.fuel)[0][0]
         return np.where(molar_conversion[:, fuel_index] <= self.conversion_percent)[0]
 
+    def generate_rate_of_production(self, gas):
+        rate_of_production = defaultdict(
+            sps.lil_matrix((len(self.conversion_indices), self.n_reactions))
+        )
+
+        for j in range(len(self.conversion_indices)):
+            gas.TPY = self.temperature[j], self.pressure[j], self.mass_fractions[j, :]
+            rop = gas.net_rates_of_progress*self.stoich_diff
+            for o, k in enumerate(self.species_names):
+                rate_of_production[k][j, :] = rop[o, :]
+
+        return rate_of_production
+
     def generate_table(self):
-        gas = Solution(self.chem_file_name)
-        self.n_reactions = gas.n_reactions
-        self.reaction_equations = gas.reaction_equations()
-        self.species_names = gas.species_names
+        integ_rop = DataFrame(index=self.reaction_equations, columns=self.species_names,
+                              dtype=np.float64)
 
-        stoich_diff = gas.product_stoich_coeffs() - gas.reactant_stoich_coeffs()
-        mole_fractions = self.get_mole_fractions(gas)
-        conversion_indices = self.get_conversion_indices(mole_fractions)
+        for spec in self.species_names:
+            integ_rop[spec] = np.trapz(y=self.rate_of_production[spec].todense(),
+                                       x=self.time[:len(self.conversion_indices)], axis=0).T
 
-        integrated_rop = np.zeros((len(self.species_names), self.n_reactions))
+        total_prod = (integ_rop[integ_rop > 0].values.sum(axis=0))/100
+        total_dest = -(integ_rop[integ_rop < 0].values.sum(axis=0))/100
 
-        for i in range(len(conversion_indices)-1):
-            gas.TPY = self.temperature[i], self.pressure[i], self.mass_fractions[i, :]
-            rop_i = gas.net_rates_of_progress*stoich_diff
-
-            gas.TPY = self.temperature[i+1], self.pressure[i+1], self.mass_fractions[i+1, :]
-            rop_ip = gas.net_rates_of_progress*stoich_diff
-
-            integrated_rop += (self.time[i+1] - self.time[i])*(rop_ip + rop_i)/2
-
-        integrated_prod = np.where(integrated_rop > 0, integrated_rop, np.zeros(integrated_rop.shape))
-        integrated_dest = np.where(integrated_rop < 0, integrated_rop, np.zeros(integrated_rop.shape))
-        total_prod = np.sum(integrated_prod, axis=1)
-        total_dest = -np.sum(integrated_dest, axis=1)
-
-        with np.errstate(divide='ignore', invalid='ignore'):
-            percent_prod = (integrated_prod.T/total_prod*100).T
-            percent_prod[percent_prod == np.inf] = 0
-            percent_prod = np.nan_to_num(percent_prod)
-            percent_dest = (integrated_dest.T/total_dest*100).T
-            percent_dest[percent_dest == np.inf] = 0
-            percent_dest = np.nan_to_num(percent_dest)
-
-        self.percent_combined = percent_prod + percent_dest
-        self.df = DataFrame(
-            np.hstack((np.array((range(1, self.n_reactions+1),)).T, self.percent_combined.T)),
-            index=self.reaction_equations, columns=(['Reaction No.'] + self.species_names)
-            )
-
-    def write_output(self):
-        self.df.to_excel(self.output_file_name)
+        integ_rop[integ_rop > 0] /= total_prod
+        integ_rop[integ_rop < 0] /= total_dest
+        return integ_rop
