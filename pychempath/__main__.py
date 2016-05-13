5e2d6bcd37b7cc65b3e404475322b2765b5715df
Bryan W. Weber <bryan@bryanwweber.com>
2016-05-13 16:16:17 -0400

    Read the CTI file from the save file if unspecified
    The CTI file can be stored in the save file as a UTF-8 character array.
    See bryanwweber/CanSen#41

--- pychempath/__main__.py
+++ pychempath/__main__.py
@@ -6,7 +6,7 @@ def main():
     parser = arg.ArgumentParser(
         description="Create a path analysis file from a CanSen output file"
         )
-    parser.add_argument('-c', '--chem', help='Specify the chemistry file', default='gri30.cti')
+    parser.add_argument('-c', '--chem', help='Specify the chemistry file')
     parser.add_argument('-s', '--save', help='Specify the stored save file', default='save.hdf')
     parser.add_argument('-o', '--output', help='Specify the output file', default='output.xlsx')
     parser.add_argument('-p', '--percent', help='Specify the integration percent',
@@ -15,11 +15,11 @@ def main():
     args = parser.parse_args()
 
     PathAnalysis(
-        chem=args.chem,
         save=args.save,
         output=args.output,
         percent=args.percent,
         fuel=args.fuel,
+        chem=args.chem,
         )
 
 if __name__ == '__main__':
